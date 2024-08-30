bb59b579f06d5700331a71d12a8a1013ae939638
kahindiagenisys <kah@indiagenisys.com>
2024-08-30 18:09:26 +0530

    drift database init

---
+++ lib/database/daos/product_dao.dart
@@ -0,0 +1,14 @@
+// import 'package:billingapp/database/database.dart';
+// import 'package:billingapp/database/tables/product.dart';
+// import 'package:drift/drift.dart';
+//
+// part 'product_dao.g.dart';
+//
+//
+// @DriftAccessor(tables: [Product])
+// class ProductDao extends DatabaseAccessor<AppDatabase> with _$ProductDaoMixin {
+//
+//   ProductDao(AppDatabase db) : super(db);
+//
+//
+// }
